49cab02ee7e37fdc4a56ddd7defba33827121ef5
David Peguero <david.peguero.cancel@gmail.com>
2023-03-31 20:34:46 -0400

    Added ability to change question (answering still being implemented)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 //Current question index
-qIndex = 0; 
+var qIndex = 0; 
 
 var mainContent = document.querySelector(".main-content");
-
+var currentQuestion = {}
 const playContent = 
 '<div>Would you like to play a game</div> <button type="submit" id="play">Play</button>'
 
@@ -22,7 +22,19 @@ const question2 = {
     correctAnswer : "word"
 }
 
-const qArray = [question1, question2];
+const question3 = {
+    question : "What is a boolean?",
+    answers : ["0","word", "true", "undefined"],
+    correctAnswer : "true"
+}
+
+const question4 = {
+    question : "What is undefined?",
+    answers : ["0","undefined", "true", "undefined"],
+    correctAnswer : "undefined"
+}
+
+const qArray = [question1, question2, ];
 
 function swapContent(newinnerHTML){
     mainContent.innerHTML = newinnerHTML;
@@ -37,11 +49,43 @@ playButton.addEventListener("click", playGame);
 //Function that starts the game
 function playGame(){
     swapContent(questionFormat)
-    var answers = document.querySelectorAll('.answer');
-    var question = document.querySelector('.question')
-
-    while(qIndex < qArray.length){
-        let currentQuestion = qArray[qIndex];
-        
-    }
+    presentQuestion() 
+       
 }
+
+
+function presentQuestion(){
+    
+    if(qIndex === qArray.length){
+        return;
+    }
+    //Question we are currently displaying
+    let answers = document.querySelectorAll('.answer');
+    let question = document.querySelector('.question');
+    var answerList = document.querySelector(".answers");
+    currentQuestion = qArray[qIndex];
+    console.log(currentQuestion);
+    question.innerHTML = currentQuestion.question;
+    for(var i = 0; i < answers.length; i++){
+        answers[i].innerHTML = currentQuestion.answers[i];
+    }
+
+    
+    answerList.addEventListener("click", giveAnswer = function(e){
+        var element = e.target;
+        if(element.matches(".answer")){
+            console.log(element.innerHTML);
+            console.log(currentQuestion.correctAnswer)
+            console.log("same? " + element.innerHTML === currentQuestion.correctAnswer)
+            if(element.innerHTML === currentQuestion.correctAnswer){
+                answerList.removeEventListener("click", giveAnswer);
+                qIndex++;
+                presentQuestion();
+            }
+        }
+    })
+}
+
+
+
+
